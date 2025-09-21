f50474faa5fb210f4dc18762d0d752310a0dfc57
nemheen <40517906+nemheen@users.noreply.github.com>
2025-09-22 00:16:10 +0900

    Update firebase_init.py

--- backend/firebase_init.py
+++ backend/firebase_init.py
@@ -2,8 +2,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-def get_db():
+def init_firebase_app():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("/Users/nemekhbayarnomin/Documents/Intern/mof/mof-application/mof-firebase-b6b3b-firebase-adminsdk-fbsvc-f23d58405d.json")
+        cred = credentials.Certificate("/service_account.json")
         firebase_admin.initialize_app(cred)
     return firestore.client()
+
